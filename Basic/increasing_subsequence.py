c85b71c92c0a002e569d15b99e4a6869bd16c6f2
Alice Scarpa <alicescarpa@gmail.com>
2015-11-01 15:32:35 -0200

    More lis algos

--- Basic/increasing_subsequence.py
+++ Basic/increasing_subsequence.py
@@ -1,3 +1,4 @@
+import bisect
 from bit import Bit
 
 
@@ -33,9 +34,49 @@ def lis_bit(array):
         bit.update(num, a + 1)
     return bit.compute(M)
 
+
+def lis_traditional(array):
+    """
+    If we want to have a list such that list[i] is the
+    smallest ending for a increasing subsequence of size i
+    We can iterate for every num in the array finding where to
+    update to keep the property:
+    1 -> x1
+    2 -> x2
+          num
+    3 -> x3
+    We can update to:
+    1 -> x1
+    2 -> x2
+    3 -> num
+    """
+    M = min(len(array), max(array)) + 1
+    # smallest_val[i] = smallest ending of a increasing subsequence of len i
+    smallest_val = [M]*M
+    smallest_val[0] = 0
+    # smallest_idx[i] = index of the smallest ending of an inc subseq of len i
+    smallest_idx = [-1]*M
+    prev = [None]*len(array)
+    for i, num in enumerate(array):
+        index = bisect.bisect_left(smallest_val, num)
+        smallest_val[index] = num
+        smallest_idx[index] = i
+        prev[i] = smallest_idx[index - 1]
+    last = smallest_idx[bisect.bisect_left(smallest_val, M) - 1]
+    ans = []
+    while last != -1:
+        ans.append(array[last])
+        last = prev[last]
+    return ans[::-1]
+
+
 print lis([1, 2, 3, 4, 5, 6])
 print lis([1, 2, 3, 4, 5, 6, 1, 2, 3, 4])
 print lis([1, 4, 2, 3, 3, 2, 4, 1, 5, 6])
 print lis_bit([1, 2, 3, 4, 5, 6])
 print lis_bit([1, 2, 3, 4, 5, 6, 1, 2, 3, 4])
 print lis_bit([1, 4, 2, 3, 3, 2, 4, 1, 5, 6])
+print lis_traditional([1, 2, 3, 4, 5, 6])
+print lis_traditional([5, 4, 3, 2, 1])
+print lis_traditional([1, 2, 3, 4, 5, 6, 1, 2, 3, 4])
+print lis_traditional([1, 4, 2, 3, 3, 2, 4, 1, 5, 6])
